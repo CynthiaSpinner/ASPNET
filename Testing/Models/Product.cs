bef4022668224a5504cf8810621985f46960d970
Cynthia <CynthiaSpinner@gmail.com>
2024-05-02 17:35:45 -0500

    working on css. need to do product page and io/xo

--- Testing/Models/Product.cs
+++ Testing/Models/Product.cs
@@ -1,4 +1,7 @@
-﻿namespace Testing.Models
+﻿using System;
+using System.Collections.Generic;
+
+namespace Testing.Models
 {
     public class Product
     {
@@ -10,5 +13,6 @@
         public int CategoryID { get; set; }
         public int OnSale { get; set; }
         public int StockLevel { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
     }
 }
